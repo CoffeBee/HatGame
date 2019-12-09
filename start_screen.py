6aa927f869a6edd84caab016612be4de7bf52311
Anna Khoroshikh <annakhoroshikh@MacBook-Air-Anna.local>
2019-12-09 09:59:30 +0300

    start_screen

--- start_screen.py
+++ start_screen.py
@@ -10,9 +10,21 @@ class StartScreen(Screen):
     """No ideas"""
     def __init__(self, **kwargs):
         super(StartScreen, self).__init__(**kwargs)
-        root_box = BoxLayout(orientation='horizontal')
-        root_box.add_widget(Button(text='Начать игру', on_press=self.start_callback))
+        root_box = FloatLayout(orientation='horizontal')
+        root_box.add_widget(self.build())
         self.add_widget(root_box)
+
+    def build(self): 
+        """ use a (r, g, b, a) tuple """
+        root_box = Button(text ="Начать игру!", 
+                   font_size ="20sp", 
+                   background_color =(1, 1, 1, 1), 
+                   color =(255, 1, 1, 1),  
+                   size =(32, 32), 
+                   size_hint =(.2, .2), 
+                   pos_hint={'y' : .4, 'x' : .4}) 
+ 
+        return root_box
 
     def start_callback(self, instance):
     	self.manager.current = self.manager.next()
