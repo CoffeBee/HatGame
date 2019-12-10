ef6ea875169cd677df3b20c1a9b97feb3ee596bc
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2019-12-10 21:21:07 +0300

    add start screen

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from kivy.uix.screenmanager import ScreenManager
 from players_screen import PlayerScreen
 from game_screenmanager import GameScreenManager
 from game_screen import GameScreen
+from start_screen import StartScreen
+
 class MyApp(App):
     """This is main class of app"""
     def build(self):
         """This method is build app"""
         root = GameScreenManager()
+        root.add_widget(StartScreen(name="start"))
         root.add_widget(PlayerScreen(name="players"))
         root.add_widget(GameScreen(name="players2"))
         return root
